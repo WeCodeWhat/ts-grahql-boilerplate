58d1e17b9d653a1e10e0cfc9d8bb1a6ad2a22a17
Chung Quan Tin <s3818487@rmit.edu.vn>
2020-11-18 21:04:43 +0700

    setup GraphQL server with nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { GraphQLServer } from "graphql-yoga";
 
-createConnection().then(async connection => {
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+const resolvers = {
+	Query: {
+		hello: (_: any, { name }: any) => `Hello ${name || "World"}`,
+	},
+};
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
