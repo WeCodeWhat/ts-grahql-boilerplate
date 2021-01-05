a1bb6991df51ad559a6349c0a8e80dccd9f256dc
Chung Quan Tin <s3818487@rmit.edu.vn>
2021-01-06 00:53:21 +0700

    add process manager

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # A BOILERPLATE FOR REACT-TYPESCRIPT-GRAPHQL STACK
 
+Environment Variables Setup:
+SERVER_PORT = <Your server will run on this port />
+TEST_ACCOUNT_EMAIL = <The email to test the SparkPost service />
+SESSION_SECRET = <Session secret key />
+SPARKPOST_KEY= <SparkPost API key />
+FRONTEND_HOST= <The url of the project frontend />
+TEST_HOST= <Leave blank />
+NODE_ENV= <Leave blank />
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,45 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
+	apps: [
+		{
+			name: "React Typescript GraphQL boilerplate",
+			script: "./src/index.ts",
+			instances: 2,
+			cwd: __dirname,
+			autorestart: true,
+			max_memory_restart: "1G",
+			exec_mode: "cluster",
+			env: {
+				NODE_ENV: "production",
+			},
+		},
+	],
+	deploy: {
+		production: {
+			host: "127.0.0.1",
+			user: "deploy",
+			ssh_options: ["ForwardAgent=yes"],
+			ref: "origin/master",
+			repo: "git@github:repo/repo.git",
+			path: "/path/to/project",
+			"post-deploy":
+				"cd /path/to/project && NODE_ENV=production yarn --production=false;yarn build;pm2 startOrReload ecosystem.config.js",
+			env: {
+				NODE_ENV: "production",
+			},
+		},
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+		staging: {
+			user: "deploy",
+			host: "xx.yy.zz.vv",
+			ref: "origin/develop",
+			repo: "git@github.com:repo/repo.git",
+			path: "/path/to/project",
+			ssh_options: ["PasswordAuthentication=no", "StrictHostKeyChecking=no"],
+			"post-deploy":
+				"cd /path/to/project && yarn --production=false;yarn build;pm2 startOrReload ecosystem.config.js",
+			env: {
+				NODE_ENV: "production",
+			},
+		},
+	},
 };
